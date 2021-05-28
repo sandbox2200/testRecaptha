892c1d0d1c743524da327539abc00296ec53176b
sandbox2200 <84020112+sandbox2200@users.noreply.github.com>
2021-05-28 21:17:19 +0200

    Uploaded test site

---
+++ assets/script/index.js
@@ -0,0 +1,29 @@
+let userData = {}
+
+updateForm = (name, data) => {
+    userData[name] = data;
+}
+
+sendToDb = () => {
+    // POST request with body equal on data in JSON format
+    fetch('https://nuearth-api.herokuapp.com/api/send', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+    })
+    .then((response) => response.json())
+    
+    // If error occurs then it'll be recorded
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+
+var onloadCallback = function () {
+    grecaptcha.render('html_element', {
+        'sitekey': 'your_site_key'
+    });
+};
